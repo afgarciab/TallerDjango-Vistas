401d68f48a0f9a8db844db45bc4cf6d782659859
afgarciab <af.garciab@uniandes.edu.co>
2022-02-13 12:10:22 -0500

    falta las urls de put, getID y post juntos con postman

---
+++ measurements/logic/logic_measurements.py
@@ -0,0 +1,21 @@
+from ..models import Measurement
+#retorna todos los measurements
+def get_measurements():
+    measurements = Measurement.objects.all()
+    return measurements
+
+#devuelve un measurement por id
+def get_measurement(var_pk):
+    measurement = Measurement.objects.get(pk=var_pk)
+    return measurement
+
+def create_measurement(var):
+    measurement = Measurement(name=var["name"])
+    measurement.save()
+    return measurement
+
+def update_measurement(var_pk, new_var):
+    measurement = get_measurement(var_pk)
+    measurement.name = new_var["name"]
+    measurement.save()
+    return measurement
--- measurements/views.py
+++ measurements/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
+from .logic import logic_measurements as vl
+from django.http import HttpResponse
+from django.core import serializers
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def measurements_view(request):
+    if request.method == 'GET':
+        id = request.GET.get("id", None)
+        if id:
+            measurement = vl.get_measurement(pk)
+            measurement_dto = serializers.serialize('json', measurement)
+            return HttpResponse(measurement_dto, 'application/json')
+        else:
+            measurement = vl.get_measurements()
+            measurement_dto = serializers.serialize('json', measurement)
+            return HttpResponse(measurement_dto, 'application/json')
+
+    if request.method == 'POST':
+        measurement = vl.create_measurement(json.loads(request.body))
+        measurement_dto = serializers.serialize('json', measurement)
+        return HttpResponse(measurement_dto, 'application/json')
+
+@csrf_exempt
+def measurement_view(request, pk):
+    if request.method == 'GET':
+        measurement_dto = vl.get_measurement(pk)
+        measurement = serializers.serialize('json', [measuremente_dto,])
+        return HttpResponse(measurement, 'application/json')
+
+    if request.method == 'PUT':
+        measurement_dto = vl.update_measurement(pk, json.loads(request.body))
+        measurement = serializers.serialize('json', [measurement_dto,])
+        return HttpResponse(measurement, 'application/json')
+
+
 # Create your views here.
